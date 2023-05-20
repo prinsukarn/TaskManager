6c4e787292e670be04d6e94d1ac9695b617093f6
Pratiksha Karn <karnprtksh11@gmail.com>
2023-05-20 13:34:24 +1000

    feat: add form for adding task

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,5 +1,18 @@
 import React from "react";
 
 export const AddTask = () => {
-  return <div>AddTask</div>;
+  return (
+    <section className="addTask">
+      <form>
+        <input
+          type="text"
+          name="task"
+          autoComplete="off"
+          placeholder="Add Task"
+          maxLength="50"
+        />
+        <button type="submit">Add Task</button>
+      </form>
+    </section>
+  );
 };
