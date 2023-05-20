c01de058b50a20893c824199d5927f1405e0cf2a
Pratiksha Karn <karnprtksh11@gmail.com>
2023-05-20 13:26:16 +1000

    feat: add header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,21 @@
 import React from "react";
+import logo from "../assets/TaskManager.png";
 
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="logo">
+        <img src={logo} alt="TaskManager Logo" />
+        <span>Taskmanager</span>
+      </div>
+      <div className="themeSelector">
+        <span className="light "></span>
+        <span className="medium activeTheme"></span>
+        <span className="dark"></span>
+        <span className="gradient1"></span>
+        <span className="gradient2"></span>
+        <span className="gradient3"></span>
+      </div>
+    </header>
+  );
 };
