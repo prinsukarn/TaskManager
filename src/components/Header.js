f87a7c8fd48fe4e11982874b167e4314999df3c3
Pratiksha Karn <karnprtksh11@gmail.com>
2023-05-20 15:21:53 +1000

    feat: change theme and save in localStorage

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "../assets/TaskManager.png";
 
 export const Header = () => {
+  const [theme, setTheme] = useState(
+    JSON.parse(localStorage.getItem("theme" || "dark"))
+  );
+
+  useEffect(() => {
+    localStorage.setItem("theme", JSON.stringify(theme));
+    document.documentElement.removeAttribute("class");
+    document.documentElement.classList.add(theme);
+  }, [theme]);
+
   return (
     <header>
       <div className="logo">
@@ -9,12 +19,36 @@ export const Header = () => {
         <span>Taskmanager</span>
       </div>
       <div className="themeSelector">
-        <span className="light "></span>
-        <span className="medium activeTheme"></span>
-        <span className="dark"></span>
-        <span className="gradient1"></span>
-        <span className="gradient2"></span>
-        <span className="gradient3"></span>
+        <span
+          className={theme === "light" ? "light activeTheme" : "light"}
+          onClick={() => setTheme("light")}
+        ></span>
+        <span
+          className={theme === "medium" ? "medium activeTheme" : "medium"}
+          onClick={() => setTheme("medium")}
+        ></span>
+        <span
+          className={theme === "dark" ? "dark activeTheme" : "dark"}
+          onClick={() => setTheme("dark")}
+        ></span>
+        <span
+          className={
+            theme === "gradient1" ? "gradient1 activeTheme" : "gradient1"
+          }
+          onClick={() => setTheme("gradient1")}
+        ></span>
+        <span
+          className={
+            theme === "gradient2" ? "gradient2 activeTheme" : "gradient2"
+          }
+          onClick={() => setTheme("gradient2")}
+        ></span>
+        <span
+          className={
+            theme === "gradient3" ? "gradient3 activeTheme" : "gradient3"
+          }
+          onClick={() => setTheme("gradient3")}
+        ></span>
       </div>
     </header>
   );
