3d3ad58796adb029eb307762bda2b38981fe07e1
Pratiksha Karn <karnprtksh11@gmail.com>
2023-05-20 13:52:06 +1000

    feat: add tasklist container and tasks

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,5 +1,32 @@
 import React from "react";
 
 export const TaskList = () => {
-  return <div>TaskList</div>;
+  const tasks = [
+    { id: 1001, name: "Task 1", time: "01:37:00 PM 20/05/2023" },
+    { id: 1002, name: "Task 2", time: "01:37:00 PM 20/05/2023" },
+    { id: 1003, name: "Task 3", time: "01:37:00 PM 20/05/2023" },
+  ];
+  return (
+    <section className="taskList">
+      <p className="head">
+        <span>
+          <span className="title">Todo</span>
+          <span className="count">0</span>
+        </span>
+        <span className="clearAll">Clear All</span>
+      </p>
+      <ul>
+        {tasks.map((task) => (
+          <li>
+            <p>
+              <span className="taskName"> {task.name}</span>
+              <span className="taskTime">{task.time}</span>
+            </p>
+            <i className="bi bi-pencil-square"></i>
+            <i className="bi bi-trash-fill"></i>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 };
